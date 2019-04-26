a3f438a2c8177184e07882c026e67cfde06bd3e7
d2porto <porto.7@hotmail.com>
2019-04-25 22:04:06 -0300

    detalhes do produto

--- index.php
+++ index.php
@@ -318,15 +318,29 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 
 $app->get("/categories/:idcategory", function($idcategory){
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
 	$category = new Category();
 
 	$category->get((int)$idcategory);
 
+	$pagination = $category->getProductsPage($page);
+
+	$pages = [];
+
+	for ($i=1; $i <= $pagination['pages']; $i++) {
+		array_push($pages, [
+			'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
+			'page'=>$i
+		]);
+	}
+
 	$page = new Page();
 
 	$page->setTpl("category", [
-		"category"=>$category->getValues(),
-		"products"=>Product::checkList($category->getProducts())
+		'category'=>$category->getValues(),
+		'products'=>$pagination["data"],
+		'pages'=>$pages
 	]);
 
 });
@@ -473,6 +487,21 @@ $app->get("/admin/categories/:idcategory/products/:idproduct/remove", function($
 	header("Location: /admin/categories/".$idcategory."/products");
 	exit;
 	
+});
+
+$app->get("/products/:desurl", function($desurl){
+
+	$product = new Product();
+
+	$product->getFromURL($desurl);
+
+	$page = new Page();
+
+	$page->setTpl("product-detail", [
+		'product'=>$product->getValues(),
+		'categories'=>$product->getCategories()
+	]);
+
 });
 
 $app->run();
